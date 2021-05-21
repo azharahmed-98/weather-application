57f9a64654a1b42546f7f8d966e0b521f716af7e
Azhar Ahmed A <Azhar.AhmedA@finastra.com>
2021-05-21 20:48:31 +0530

    Basic Styling and Adding the Assets

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <div id="app">
-
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Enter your City" />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return{
+      apiKey: '653cb2ac019d0a5e7af7f1bcf283ca4c'
+    }
+  }
 }
 </script>
 
