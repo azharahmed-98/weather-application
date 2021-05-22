538b1d75c88c3c1ded642b7d792dad0efd8d73fe
Azhar Ahmed A <Azhar.AhmedA@finastra.com>
2021-05-22 18:27:16 +0530

    Hard Coded Weather and Dates with required Styles.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,17 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Enter your City" />
       </div>
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">Bangalore, India</div>
+          <div class="date">5 May, 2021</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">9°c</div>
+          <div class="weather">Rain</div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -14,7 +25,10 @@ export default {
   name: 'App',
   data () {
     return{
-      apiKey: '653cb2ac019d0a5e7af7f1bcf283ca4c'
+      apiKey: '653cb2ac019d0a5e7af7f1bcf283ca4c',
+      baseUrl: 'http://api.openweathermap.org/data/2.5/',
+      query: ',',
+      weather: {}
     }
   }
 }
@@ -42,5 +56,75 @@ body {
 main{
   min-height: 100vh;
   padding: 25px;
+
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
+.search-box{
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
 </style>
